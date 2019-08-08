a40169425ff7341893a311ddaac325643b6bad38
mcbsanchez <53814143+mcbsanchez@users.noreply.github.com>
2019-08-08 10:48:44 +0800

    Add files via upload

--- Parcel.java
+++ Parcel.java
@@ -1,71 +1,145 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Parcel {
-    private double[][] size = {{9, 14, 1}, {12, 18, 3}, {12, 10, 5}, {14, 11, 7}, {18, 12, 9}, {20, 16, 12}};
-    private boolean[] AVAILABLE_PARCELS = {false,false,false,false,false,false};
+    private boolean[] available = {false,false,false,false,false,false};
     private static String[] CURRENTSTATUS = {"PREPARING","SHIPPING","DELIVERED"};
     private static String[] PARCELTYPE = {"FLAT","BOX"};
+    private String recipient;
+    private String destination;
+    private int countItems;
 
 
-    public Parcel (String destination)
-    {
-        if( destination.equalsIgnoreCase("Metro Manila") ||
-            destination.equalsIgnoreCase("Provincial Luzon") ||
-            destination.equalsIgnoreCase("Visayas") ||
-            destination.equalsIgnoreCase("Mindanao"))
-            this.destination = destination;
-        else
-            System.out.println("Error, Invalid Location, setting default to " + (this.destination = "Metro Manila"));
+    public Parcel (String recipient, String destination, int countItems) {
+        this.recipient = recipient;
+        this.destination = destination;
+        this.countItems = countItems;
     }
-    public void addItems(ArrayList<Item> items)
-    {
-        int i,j;
-        double l,w,h;
-        for(i = 0; i < items.size(); i++)
-        {
-            l = items.get(i).getLength();
-            w = items.get(i).getWidth();
-            if (items.get(i) instanceof Document)
+
+    public void addItem(ArrayList<Item> items, double[][]size) {
+        /*
+        i - number of spaces
+        j - number of rotations
+        k - number of sides
+        c - number of difference above negative
+         */
+        int i, j, k, c = 0;
+        boolean tempAvailable[] = {false, false, false, false, false, false};
+        if (items.size() == 1)
+            if (!isTooHeavy(items))
             {
-                h = ((Document) items.get(i)).getHeight();
+                for (i = 0; i < 2; i++)
+                    for (j = 0; j < 6; j++)
+                    {
+                        for (k = 0; k < 3; k++)
+                        {
+                            if (size[i][k] > items.get(i).rotate(j)[k])
+                                c++;
+                            if (c == 3)
+                                available[i] = true;
+                        }
+                        c = 0;
+                    }
             }
             else
             {
-                h = ((Product) items.get(i)).getHeight();
+                for (i = 2; i < 6; i++)
+                    for (j = 0; j < 6; j++)
+                    {
+                        for (k = 0; k < 3; k++)
+                        {
+                            if (size[i][k] > items.get(i).rotate(j)[k])
+                                c++;
+                            if (c == 3)
+                                available[i] = true;
+                        }
+                        c = 0;
+                    }
             }
-            items.get(i)
-        }
-    }
-    private double accountWeight(Item items)
-    {
-
-    }
-    public double[][] createTemporarySizes(double[][] prevSize, int row, Item item, int orientation)
-    {
-        double[][] nextSize;
-        if(item instanceof Document)
-        {
-            nextSize = new double[][]{{prevSize[row][0] - ((Document) item).rotate(orientation)[0], prevSize[row][1], prevSize[row][2]},
-                    {prevSize[row][0], prevSize[row][1] - ((Document) item).rotate(orientation)[1], prevSize[row][2]},
-                    {prevSize[row][0], prevSize[row][1], prevSize[row][2] - ((Document) item).rotate(orientation)[2]}};
-        }
         else
         {
-            nextSize = new double[][]{{prevSize[row][0] - ((Product) item).rotate(orientation)[0], prevSize[row][1], prevSize[row][2]},
-                    {prevSize[row][0], prevSize[row][1] - ((Product) item).rotate(orientation)[1], prevSize[row][2]},
-                    {prevSize[row][0], prevSize[row][1], prevSize[row][2] - ((Product) item).rotate(orientation)[2]}};
+            if (!isTooHeavy(items))
+            {
+                for (i = 0; i < 2; i++)
+                {
+                    for (j = 0; j < 6; j++)
+                    {
+                        for (k = 0; k < 3; k++)
+                        {
+                            if (size[i][k] > items.get(i).rotate(j)[k])
+                                c++;
+                            if (c == 3)
+                                tempAvailable[j] = true;
+                        }
+                        c = 0;
+                    }
+                    for (j = 0; j < 6; j++) {
+                        if (tempAvailable[j])
+                            available[i] = isFit(createNewSpace(size, i, items.get(i).rotate(j)), items);
+                        tempAvailable[j] = false;
+                    }
+                }
+            }
+            else{
+                for (i = 2; i < 6; i++)
+                {
+                    for (j = 0; j < 6; j++)
+                    {
+                        for (k = 0; k < 3; k++)
+                        {
+                            if (size[i][k] > items.get(i).rotate(j)[k])
+                                c++;
+                            if (c == 3)
+                                tempAvailable[j] = true;
+                        }
+                        c = 0;
+                    }
+                    for (j = 0; j < 6; j++) {
+                        if (tempAvailable[j])
+                            available[i] = isFit(createNewSpace(size, i, items.get(i).rotate(j)), items);
+                        tempAvailable[j] = false;
+                    }
+                }
+            }
         }
-        return nextSize;
+
+    }
+    private double[][]createNewSpace(double[][] size,int i, double[] item)
+    {
+        return new double[][]{  {size[i][0] - item[0], size[i][1], size[i][2]},
+                                {size[i][0], size[i][1] - item[1], size[i][2]},
+                                {size[i][0], size[i][1], size[i][2] - item[2]}};
+    }
+    private boolean isFit(double[][] size, ArrayList<Item> items)
+    {
+        int i, j, k, c = 0;
+        boolean condition = false;
+        if(items.size() == 0)
+            condition = true;
+        else
+        {
+            items.remove(0);
+            for(i = 0; i < 3; i++)
+                for(j = 0; j < 6; j++)
+                    for(k = 0; k < 3; k++)
+                    {
+                        if ((size[i][k] > items.get(i).rotate(j)[k]))
+                            c++;
+                        if (c == 3)
+                            condition = isFit(createNewSpace(size,i,items.get(i).rotate(j)),items);
+                    }
+        }
+        return condition;
+
     }
 
-
-
-
-    private String destination;
-    private ArrayList<Item> items = new ArrayList<>();
-
-    //length width and height
-
-    private double[] size = new double[3];
+    private boolean isTooHeavy(ArrayList<Item> items)
+    {
+        int i;
+        double totalWeight = 0;
+        for(i = 0; i < items.size(); i++)
+        {
+            totalWeight += items.get(i).getWeight();
+        }
+        return totalWeight > 3;
+    }
 }

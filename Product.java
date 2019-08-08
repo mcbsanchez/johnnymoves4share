5455933d391e62fa46cf6aead9116af37dd2f7fb
mcbsanchez <mcbsanchez@users.noreply.github.com>
2019-08-08 22:25:16 +0800

    Update Product.java

--- Product.java
+++ Product.java
@@ -18,28 +18,5 @@ public class Product extends Item
     }
     private double height;
     private double weight;
-    /*
-    to be used by loop method in fitItem
-    0 - l,w,h ; 1 - h,l,w ; 2 - h,w,l
-    3 - w,l,h ; 4 - l,h,w ; 5 - w,h,l
-    */
-    public double[] rotate(int orientation)
-    {
-        double l = getLength();
-        double h = getHeight();
-        double w = getWidth();
-        double[] dimensions = new double[]{l,w,h};
-        if(orientation == 1)
-            dimensions = new double[]{h, l, w};
-        else if(orientation == 2)
-            dimensions = new double[]{h, w, l};
-        else if(orientation == 3)
-            dimensions = new double[]{w, l, h};
-        else if(orientation == 4)
-            dimensions = new double[]{l, h, w};
-        else if(orientation == 5)
-            dimensions = new double[]{w, h, l};
-        return dimensions;
 
-    }
 }

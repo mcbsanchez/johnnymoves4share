f9aaebfbf4dafb85af6f6b5c73d795f994390433
mcbsanchez <mcbsanchez@users.noreply.github.com>
2019-08-09 14:02:53 +0800

    Revised coding

--- Driver.java
+++ Driver.java
@@ -6,14 +6,13 @@ public class Driver {
     public static void main(String[] args)
     {
         Calendar cal = Calendar.getInstance();
-        double[][] size = {{9, 14, 1}, {12, 18, 3}, {12, 10, 5}, {14, 11, 7}, {18, 12, 9}, {20, 16, 12}};
 
         ArrayList<Item> items = new ArrayList<>();
         items.add(new Document(4,4,1));
         items.add(new Product(4,4,1,1));
         Parcel parcel = new Parcel("Martin","Metro Manila",items.size());
         parcel.addItem(items);
-        parcel.showAvailableParcels(size);
+        parcel.showAvailableParcels();
         parcel.setType(1);
         parcel.setType(2);
         parcel.setType(3);

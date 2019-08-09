88e3a849af920281db1d5447e807078d0962a944
mcbsanchez <mcbsanchez@users.noreply.github.com>
2019-08-09 18:18:24 +0800

    unfinished driver

--- Driver.java
+++ Driver.java
@@ -3,27 +3,39 @@ import java.util.Calendar;
 
 public class Driver {
 
+    Calendar cal = Calendar.getInstance();
+    public void simulateTime()
+    {
+        Thread clock = new Thread() {
+            public void run()
+            {
+                for(;;){
+                    cal.add(Calendar.HOUR, 1);
+                    try{
+                        sleep(1000);
+                    }catch (InterruptedException ex)
+                    {
+                    }
+                }
+            }
+        };
+        clock.start();
+    }
     public static void main(String[] args)
     {
-        Calendar cal = Calendar.getInstance();
-
         ArrayList<Item> items = new ArrayList<>();
         items.add(new Document(4,4,1));
-        items.add(new Product(4,4,1,1));
-        Parcel parcel = new Parcel("Martin","Metro Manila",items.size());
+        items.add(new Product(4,4,1,100));
+        items.add(new Irregular(4,4,1,1));
+        Parcel parcel = new Parcel("Martin","Visayas",items.size());
+
         parcel.addItem(items);
         parcel.showAvailableParcels();
-        parcel.setType(1);
-        parcel.setType(2);
         parcel.setType(3);
-        parcel.setType(4);
-        parcel.setType(5);
-        parcel.setType(7);
-        parcel.setType(0);
         System.out.println(parcel.getType());
+        System.out.println("L "+parcel.getDimensions()[0]+" W "+parcel.getDimensions()[1]+" H "+parcel.getDimensions()[2] + " WEIGHT " + parcel.getTotalWeight());
         parcel.setInsurance("Yes");
         parcel.showBreakdown();
-        cal.add(Calendar.DAY_OF_YEAR, 1);
 
 
     }
